b69d5fc57c5ee697a771edfd7861c4f9dc5f9f6f
stu <stu@DESKTOP-NRPT3V0>
2022-01-19 13:40:22 +0900

    두번째커밋

--- src/com/itwill/shop/Member.java
+++ src/com/itwill/shop/Member.java
@@ -1,5 +1,10 @@
 package com.itwill.shop;
 
 public class Member {
+	private int no;
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+	
 
 }
--- src/com/itwill/shop/MemberDao.java
+++ src/com/itwill/shop/MemberDao.java
@@ -1,5 +1,8 @@
 package com.itwill.shop;
 
 public class MemberDao {
+	public void insert() {//파일을 변경하면 modified 상태 파일에 >가표시됨
+		
+	}
 
 }
